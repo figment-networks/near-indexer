48470fa404812c0ae80fa69c0bf3e3041250385d
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-10-27 15:18:18 -0500

    Setup height records purge

--- pipeline/cleanup.go
+++ pipeline/cleanup.go
@@ -15,16 +15,27 @@ func RunCleanup(cfg *config.Config, db *store.Store) error {
 
 	maxHeight := uint64(h.Height) - uint64(cfg.CleanupThreshold)
 	if maxHeight == 0 {
+		log.Println("nothing to cleanup")
 		return nil
 	}
 
-	log.Println("removing run records")
-	if err := db.Runs.Cleanup(maxHeight); err != nil {
+	log.Println("starting cleanup, max height:", maxHeight, "threshold:", cfg.CleanupThreshold)
+
+	if numRows, err := db.Runs.Cleanup(maxHeight); err == nil {
+		log.Println("runs removed:", numRows)
+	} else {
 		log.Println("runs cleanup error:", err)
 	}
 
-	log.Println("removing validator records")
-	if err := db.Validators.Cleanup(maxHeight); err != nil {
+	if numRows, err := db.Heights.Cleanup(maxHeight); err == nil {
+		log.Println("heights removed:", numRows)
+	} else {
+		log.Println("heights cleanup error:", err)
+	}
+
+	if numRows, err := db.Validators.Cleanup(maxHeight); err == nil {
+		log.Println("validators removed:", numRows)
+	} else {
 		log.Println("validators cleanup error:", err)
 	}
 
