51259f8c49b509b6d7b37ff8de01453004c6948b
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-12-17 11:34:57 -0600

    Fix time test

--- model/util/time_test.go
+++ model/util/time_test.go
@@ -11,24 +11,24 @@ func TestParseTime(t *testing.T) {
 	assert.Equal(t, int64(0), ParseTime(0).Unix())
 	assert.Equal(t, int64(1596166782), ParseTime(1596166782911378000).Unix())
 	assert.Equal(t, int64(1596166782911378000), ParseTime(1596166782911378000).UnixNano())
-	assert.Equal(t, "2020-07-30T22:39:42-05:00", ParseTime(1596166782911378000).Format(time.RFC3339))
-	assert.Equal(t, "2020-07-30T22:39:42.911378-05:00", ParseTime(1596166782911378000).Format(time.RFC3339Nano))
+	assert.Equal(t, "2020-07-31T03:39:42Z", ParseTime(1596166782911378000).UTC().Format(time.RFC3339))
+	assert.Equal(t, "2020-07-31T03:39:42.911378Z", ParseTime(1596166782911378000).UTC().Format(time.RFC3339Nano))
 }
 
 func TestHourInterval(t *testing.T) {
-	now, err := time.Parse(time.RFC3339, "2020-07-30T22:39:42-05:00")
+	now, err := time.Parse(time.RFC3339, "2020-07-30T22:39:42Z")
 	assert.NoError(t, err)
 
 	start, end := HourInterval(now)
-	assert.Equal(t, "2020-07-30T22:00:00-05:00", start.Format(time.RFC3339))
-	assert.Equal(t, "2020-07-30T22:59:59-05:00", end.Format(time.RFC3339))
+	assert.Equal(t, "2020-07-30T22:00:00Z", start.Format(time.RFC3339))
+	assert.Equal(t, "2020-07-30T22:59:59Z", end.Format(time.RFC3339))
 }
 
 func TestDayInterval(t *testing.T) {
-	now, err := time.Parse(time.RFC3339, "2020-07-30T22:39:42-05:00")
+	now, err := time.Parse(time.RFC3339, "2020-07-30T22:39:42Z")
 	assert.NoError(t, err)
 
 	start, end := DayInterval(now)
-	assert.Equal(t, "2020-07-30T00:00:00-05:00", start.Format(time.RFC3339))
-	assert.Equal(t, "2020-07-30T23:59:59-05:00", end.Format(time.RFC3339))
+	assert.Equal(t, "2020-07-30T00:00:00Z", start.Format(time.RFC3339))
+	assert.Equal(t, "2020-07-30T23:59:59Z", end.Format(time.RFC3339))
 }
