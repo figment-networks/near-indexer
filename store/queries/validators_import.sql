e1454f8b02c7f86e2db0d7a7d5d4bac887378481
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-01-08 19:23:44 -0600

    Fix formatting

--- store/queries/validators_import.sql
+++ store/queries/validators_import.sql
@@ -1,15 +1,15 @@
 INSERT INTO validators (
-	height,
-	time,
-	account_id,
-	epoch,
-	expected_blocks,
-	produced_blocks,
-	slashed,
-	stake,
-	efficiency,
+  height,
+  time,
+  account_id,
+  epoch,
+  expected_blocks,
+  produced_blocks,
+  slashed,
+  stake,
+  efficiency,
   reward_fee,
-	created_at,
-	updated_at
+  created_at,
+  updated_at
 )
 VALUES @values
