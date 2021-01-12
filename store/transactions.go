2019bceef3b8a41151c777d60bbc2194938dab6b
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-01-11 20:27:14 -0600

    Add transaction search

--- store/transactions.go
+++ store/transactions.go
@@ -54,6 +54,64 @@ func (s TransactionsStore) Recent(n int) ([]model.Transaction, error) {
 	return result, checkErr(err)
 }
 
+// Search performs a transaction search and returns matching records
+func (s TransactionsStore) Search(search TransactionsSearch) (*PaginatedResult, error) {
+	if err := search.Validate(); err != nil {
+		return nil, err
+	}
+
+	scope := s.db.
+		Model(&model.Transaction{}).
+		Order("id DESC").
+		Limit(search.Limit)
+
+	if search.BlockHash != "" {
+		scope = scope.Where("block_hash = ?", search.BlockHash)
+	}
+	if search.BlockHeight > 0 {
+		scope = scope.Where("height = ?", search.BlockHeight)
+	}
+	if search.Sender != "" {
+		scope = scope.Where("sender = ?", search.Sender)
+	}
+	if search.Receiver != "" {
+		scope = scope.Where("receiver = ?", search.Receiver)
+	}
+	if search.startTime != nil {
+		scope = scope.Where("time >= ?", search.startTime)
+	}
+	if search.endTime != nil {
+		scope = scope.Where("time <= ?", search.endTime)
+	}
+
+	var count uint
+	if err := scope.Count(&count).Error; err != nil {
+		return nil, err
+	}
+
+	transactions := []model.Transaction{}
+
+	err := scope.
+		Offset((search.Page - 1) * search.Limit).
+		Limit(search.Limit).
+		Find(&transactions).
+		Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := &PaginatedResult{
+		Page:    search.Page,
+		Limit:   search.Limit,
+		Count:   count,
+		Records: transactions,
+	}
+
+	return result.update(), nil
+}
+
+// Import imports transactions in bulk
 func (s TransactionsStore) Import(records []model.Transaction) error {
 	t := time.Now()
 
