913ea7df526958900539fe667f1bef205abe493e
eesmerdag <emiresmerdag@gmail.com>
2021-05-14 00:35:23 +0300

    reward fee fraction import

--- store/queries/validator_epochs_import.sql
+++ store/queries/validator_epochs_import.sql
@@ -7,16 +7,18 @@ INSERT INTO validator_epochs (
   produced_blocks,
   efficiency,
   staking_balance,
-  reward_fee
+  reward_fee,
+  reward_fee_fraction,
 )
 VALUES @values
 
 ON CONFLICT (account_id, epoch) DO UPDATE
 SET
-  last_height     = excluded.last_height,
-  last_time       = excluded.last_time,
-  expected_blocks = excluded.expected_blocks,
-  produced_blocks = excluded.produced_blocks,
-  efficiency      = ROUND(excluded.efficiency, 4),
-  staking_balance = excluded.staking_balance,
-  reward_fee      = COALESCE(excluded.reward_fee, validator_epochs.reward_fee)
+  last_height         = excluded.last_height,
+  last_time           = excluded.last_time,
+  expected_blocks     = excluded.expected_blocks,
+  produced_blocks     = excluded.produced_blocks,
+  efficiency          = ROUND(excluded.efficiency, 4),
+  staking_balance     = excluded.staking_balance,
+  reward_fee          = COALESCE(excluded.reward_fee, validator_epochs.reward_fee),
+  reward_fee_fraction = excluded.reward_fee_fraction
--- store/validator_aggs.go
+++ store/validator_aggs.go
@@ -109,6 +109,7 @@ func (s ValidatorAggsStore) ImportValidatorEpochs(records []model.ValidatorEpoch
 			r.Efficiency,
 			r.StakingBalance,
 			r.RewardFee,
+			r.RewardFeeFraction,
 		}
 	})
 }
