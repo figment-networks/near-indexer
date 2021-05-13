3c1940d65c167962df2815aa2207fad0ffb6b502
eesmerdag <emiresmerdag@gmail.com>
2021-05-14 00:26:36 +0300

    reward fee fraction

--- model/validator_epoch.go
+++ model/validator_epoch.go
@@ -1,22 +1,24 @@
 package model
 
 import (
+	"math/big"
 	"time"
 
 	"github.com/figment-networks/near-indexer/model/types"
 )
 
 type ValidatorEpoch struct {
-	ID             int64        `json:"-"`
-	AccountID      string       `json:"-"`
-	Epoch          string       `json:"epoch"`
-	LastHeight     types.Height `json:"last_height"`
-	LastTime       time.Time    `json:"last_time"`
-	ExpectedBlocks int          `json:"expected_blocks"`
-	ProducedBlocks int          `json:"produced_blocks"`
-	Efficiency     float64      `json:"efficiency"`
-	StakingBalance types.Amount `json:"staking_balance"`
-	RewardFee      *int         `json:"reward_fee"`
+	ID                int64        `json:"-"`
+	AccountID         string       `json:"-"`
+	Epoch             string       `json:"epoch"`
+	LastHeight        types.Height `json:"last_height"`
+	LastTime          time.Time    `json:"last_time"`
+	ExpectedBlocks    int          `json:"expected_blocks"`
+	ProducedBlocks    int          `json:"produced_blocks"`
+	Efficiency        float64      `json:"efficiency"`
+	StakingBalance    types.Amount `json:"staking_balance"`
+	RewardFee         *int         `json:"reward_fee"`
+	RewardFeeFraction *big.Int     `json:"reward_fee_fraction"`
 }
 
 func (ValidatorEpoch) TableName() string {
