ca1eee56250b8256550c6f3e37fe904c5785f1a3
eesmerdag <emiresmerdag@gmail.com>
2021-06-13 14:18:37 +0300

    remove tabs

--- store/queries/validator_epochs_import.sql
+++ store/queries/validator_epochs_import.sql
@@ -13,10 +13,10 @@ VALUES @values
 
 ON CONFLICT (account_id, epoch) DO UPDATE
 SET
-  last_height         = excluded.last_height,
-  last_time           = excluded.last_time,
-  expected_blocks     = excluded.expected_blocks,
-  produced_blocks     = excluded.produced_blocks,
-  efficiency          = ROUND(excluded.efficiency, 4),
-  staking_balance     = excluded.staking_balance,
-  reward_fee          = COALESCE(excluded.reward_fee, validator_epochs.reward_fee)
+  last_height     = excluded.last_height,
+  last_time       = excluded.last_time,
+  expected_blocks = excluded.expected_blocks,
+  produced_blocks = excluded.produced_blocks,
+  efficiency      = ROUND(excluded.efficiency, 4),
+  staking_balance = excluded.staking_balance,
+  reward_fee      = COALESCE(excluded.reward_fee, validator_epochs.reward_fee)
