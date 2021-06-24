51642a693c9af06316156e6175354314f4cba881
eesmerdag <emiresmerdag@gmail.com>
2021-06-25 00:16:07 +0300

    validator info rewards

--- store/queries/delegators_rewards.sql
+++ store/queries/delegators_rewards.sql
@@ -1,5 +1,6 @@
 SELECT
   to_char(distributed_at_time, $INTERVAL) AS interval,
+  validator_id as validator,
   SUM(reward) AS amount
 FROM
   delegator_epochs
@@ -9,6 +10,7 @@ WHERE
   AND distributed_at_time BETWEEN ? AND ?
   AND reward > 0
 GROUP BY
-  to_char(distributed_at_time, $INTERVAL)
+  to_char(distributed_at_time, $INTERVAL),
+  validator_id
 ORDER BY
   to_char(distributed_at_time, $INTERVAL)
