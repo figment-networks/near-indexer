db9f2369d40cee13e71afff4c4c8b27740f4fab6
eesmerdag <emiresmerdag@gmail.com>
2021-06-28 23:59:58 +0300

    track historical delegators sync

--- cli/cli.go
+++ cli/cli.go
@@ -68,6 +68,8 @@ func startCommand(cfg *config.Config, logger *logrus.Logger, name string) error 
 		return startWorker(cfg, logger)
 	case "sync":
 		return runSync(cfg, logger)
+	case "sync-historical-delegators":
+		return runSyncHistoricalDelegators(cfg, logger)
 	case "status":
 		return startStatus(cfg)
 	case "cleanup":
